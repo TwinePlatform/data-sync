c37181500aa6dedc473b927b01fb204e394bf910
Elias Malik <elias0789@gmail.com>
2018-07-05 16:03:21 +0100

    🚚 move field maps into "extract" directory

--- src/extract/admin/fieldMap.js
+++ src/extract/admin/fieldMap.js
@@ -26,6 +26,7 @@
  *  - survey_answers    = Retrievable via FrontlineSMS API
  *  - survey_questions  = Retrievable via FrontlineSMS API
  *  - financial_data    = Retrievable via FrontlineSMS API
+ *  - meetings          = Empty table
  */
 module.exports = {
   organisations: {
@@ -42,9 +43,9 @@ module.exports = {
     updated_at: 'organisation_modified_at',
     deleted_at: 'organisation_deleted_at',
     '360_giving_id': 'organisation_360_giving_id',
-    // OMITTED: company_number     = Unneeded
-    // OMITTED: merchant_id        = Unneeded
-    // OMITTED: community_business = Flag to indicate whether ...
+    // OMITTED: company_number     = Unimportant
+    // OMITTED: merchant_id        = Unimportant
+    // OMITTED: community_business = Flag to indicate whether ...?
     // OMITTED: type               = Determines whether member of Twine Energy programme,
     //                             = now handled via subscription types
     // OMITTED: region_id          = Handled in JOIN
@@ -56,6 +57,9 @@ module.exports = {
   sectors: {
     name: 'organisation_sector',
   },
+  genders: {
+    name: 'user_gender',
+  },
   users: {
     id: 'user_id',
     name: 'user_name',
@@ -63,68 +67,64 @@ module.exports = {
     password: 'user_password',
     phone: 'user_phone_number',
     yearOfBirth: 'user_birth_year',
-    status: 'user_status',
     created_at: 'user_created_at',
     updated_at: 'user_modified_at',
     deleted_at: 'user_deleted_at',
-    api_token: 'user_api_token',
-    // OMITTED: region_id = Handled in JOIN
-    // OMITTED: gender_id = Handled in JOIN
-    // OMITTED: role_id   = Handled in JOIN
+    organisation_id: 'fk_user_to_organisation',
+    // OMITTED: region_id      = Unimportant
+    // OMITTED: gender_id      = Handled in JOIN
+    // OMITTED: role_id        = Handled in JOIN
+    // OMITTED: remember_token = Unimportant
+    // OMITTED: api_token      = Unused
+    // OMITTED: status         = Unused
   },
   user_roles: {
     name: 'user_role_name',
   },
-  meetings: {
-    id: 'outreach_meeting_id',
-    partner: 'outreach_meeting_partner',
-    date: 'outreach_meeting_scheduled_at',
-    user_id: 'outreach_meeting_to_user_fk',
-    // OMITTED: type            = Handled in JOIN
-    // OMITTED: user_id         = Handled in JOIN
-    // OMITTED: organisation_id = Handled in JOIN
+  meeting_types: {
+    name: 'outreach_campaign_interaction_type',
   },
   activities: {
-    id: 'volunteer_activity_id',
-    name: 'volunteer_activity_name',
-    created_at: 'volunteer_activity_created_at',
-    updated_at: 'volunteer_activity_modified_at',
-    deleted_at: 'volunteer_activity_deleted_at',
+    name: 'volunteer_log_activity',
+    // OMITTED id         = Unimportant
+    // OMITTED created_at = Unimportant
+    // OMITTED updated_at = Unimportant
+    // OMITTED deleted_at = Unimportant
   },
   logs: {
     id: 'volunteer_log_id',
-    user_id: 'volunteer_log_to_user_fk',
-    activity_id: 'volunteer_log_to_volunteer_activity_fk',
+    user_id: 'fk_volunteer_log_to_user',
+    organisation_id: 'fk_volunteer_log_to_organisation',
     duration: 'volunteer_log_duration',
-    date_of_log: 'volunteer_log_created_at',
-    // OMITTED: organisation_id = Handled in JOIN
+    date_of_log: 'volunteer_log_started_at',
+    // OMITTED: activity_id = Handled in JOIN
   },
   frontlinesms_api_keys: {
     id: 'frontline_account_id',
     key: 'frontline_account_api_key',
     workspace_id: 'frontline_account_workspace_id',
-    // OMITTED: organisation_id = Handled in JOIN
+    organisation_id: 'fk_frontline_account_to_organisation',
   },
   outreaches: {
-    id: 'outreach_id',
-    date: 'outreach_scheduled_at',
-    interaction_type: 'outreach_interaction_type',
-    user_id: 'outreach_to_user_fk',
-    created_at: 'outreach_created_at',
-    updated_at: 'outreach_updated_at',
-    deleted_at: 'outreach_deleted_at',
+    id: 'outreach_campaign_id',
+    date: 'outreach_campaign_scheduled_at',
+    user_id: 'fk_outreach_campaign_to_user',
+    organisation_id: 'fk_outreach_campaign_to_organisation',
+    created_at: 'outreach_campaign_created_at',
+    updated_at: 'outreach_campaign_updated_at',
+    deleted_at: 'outreach_campaign_deleted_at',
     // OMITTED: outreach_child_type = Handled in JOIN
     // OMITTED: outreach_type       = Handled in JOIN
-    // OMITTED: organisation_id     = Handled in JOIN
+    // OMITTED: interaction_type    = Handled in JOIN
   },
   outreach_types: {
-    name: 'outreach_type',
+    name: 'outreach_campaign_type',
     // OMITTED: created_at = Unimportant
     // OMITTED: updated_at = Unimportant
     // OMITTED: deleted_at = Unimportant
   },
   outreach_child_types: {
-    name: 'outreach_child_type',
+    name: 'outreach_campaign_child_type',
     // OMITTED: created_at = Unimportant
     // OMITTED: updated_at = Unimportant
     // OMITTED: deleted_at = Unimportant
