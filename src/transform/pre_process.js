9e941ffef8133ef577dad922b752320c0bfb1a59
AstroAsh <astroaisha@gmail.com>
2018-11-22 15:40:11 +0000

    🐛 fix duplicate logs with reduce

--- src/transform/pre_process.js
+++ src/transform/pre_process.js
@@ -9,7 +9,7 @@
  */
 const { randomBytes } = require('crypto');
 const uuid = require('uuid/v4');
-const { omit, evolve } = require('ramda');
+const { omit, evolve, equals } = require('ramda');
 const regionMap = require('./region_map');
 
 
@@ -376,49 +376,55 @@ const mapToTargetSchema = (entities) => {
   });
 
   entities.volunteer_log = (entities.volunteer_log || [])
-    .filter((l) => { // Remove duplicate logs
+    .reduce((acc, l) => { // Remove duplicate logs
       const d = l.volunteer_log_started_at;
 
       if (!(d instanceof Date)) {
-        return true;
+        return [...acc, l];
       }
 
       if (d.toISOString().startsWith('2018-04-30T23:00:00.000')
           && l.volunteer_log_duration === 480
-          && l.volunteer_log_updated_at.toISOString().startsWith('2018-08-06T18:33:04')) {
-
-        return false;
+          && l.volunteer_log_updated_at.toISOString().startsWith('2018-08-06T18:33:04')
+      ) {
+        return acc;
       }
 
       if (d.toISOString().startsWith('2018-09-06T11:03:13')
-          && l.volunteer_log_updated_at.toISOString().startsWith('2018-09-07T10:03:31')) {
-
-        return false;
+          && l.volunteer_log_updated_at.toISOString().startsWith('2018-09-07T10:03:31')
+      ) {
+        return acc;
       }
 
       if (d.toISOString().startsWith('2017-12-08T')
         && l.volunteer_log_duration === 420
         && l.volunteer_log_updated_at.toISOString().startsWith('2017-12-08T15:48:44')
       ) {
-        return false;
+        return acc;
       }
 
       if (d.toISOString().startsWith('2018-09-17T15:47:01')
         && l.volunteer_log_duration === 420
       ) {
-        // deletes all 3 duplicates but there is no indentifiers
-        return false;
+        const strippedAcc = acc.map((a) => omit(['volunteer_log_id'], a));
+        const strippedLog = omit(['volunteer_log_id'], l);
+
+        const existingLog = strippedAcc.find((x) => equals(x, strippedLog));
+
+        if (existingLog) {
+          return acc;
+        }
       }
 
       if (d.toISOString().startsWith('2018-10-22T20:47:51')
         && l.volunteer_log_duration === 120
         && l.volunteer_log_created_at.toISOString().startsWith('2018-10-24T05:20:54')
       ) {
-        return false;
+        return acc;
       }
 
-      return true;
-    })
+      return [...acc, l];
+    }, [])
     .map(evolve({ volunteer_log_duration: (a) => a * 60, volunteer_log_activity: (a) => a || 'Other' }));
   return entities;
 };
